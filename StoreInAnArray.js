c210a4b44f5dddf1d72873e22a5e1ed0e0dd5da7
Mohsin Raees <160493218+MohsinRaees@users.noreply.github.com>
2024-02-22 02:23:35 +0500

    Add files via upload

---
+++ StoreInAnArray.js
@@ -0,0 +1,12 @@
+//Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.//
+var countries = [
+    "United States",
+    "Canada",
+    "United Kingdom",
+    "France",
+    "Germany",
+    "Japan",
+    "Australia",
+];
+console.log("List of Countries:");
+countries.forEach(function (country) { return console.log(country); });
---
+++ StoreInAnArray.ts
@@ -0,0 +1,14 @@
+//Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.//
+
+const countries: string[] = [
+    "United States",
+    "Canada",
+    "United Kingdom",
+    "France",
+    "Germany",
+    "Japan",
+    "Australia",
+  ];
+  
+  console.log("List of Countries:");
+  countries.forEach(country => console.log(country));
